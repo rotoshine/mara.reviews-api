85cbb80ff3492373ab9dbc76630a843daf37e47d
rotoshine <rotoshine@gmail.com>
2019-04-01 18:01:26 +0900

    first modeling

--- marareviews/api/serializers.py
+++ marareviews/api/serializers.py
@@ -1,14 +1,33 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from .models import Restaurant, Menu, MenuOption, Review
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('id', 'url', 'username', 'email', 'groups')
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class RestaurantSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Group
-        fields = ('url', 'name')
+        model = Restaurant
+        fields = ('id', 'name', 'address', 'open_time', 'close_time', 'is_dead', 'is_selectable_ingredients')
+
+
+class MenuSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ('id', 'restaurant', 'option', 'name')
+
+
+class MenuOptionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = MenuOption
+        fields = ('id', 'name')
+
+
+class ReviewSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Review
+        fields = ('id', 'restaurant', 'menu', 'title', 'content', 'score', 'is_blind')
